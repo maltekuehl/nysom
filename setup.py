1c0df6f5d2eee964f58c9d61ca3f5b52c998cad3
Malte Kuehl <malte.benedikt.kuehl@studium.uni-hamburg.de>
2022-06-23 23:31:03 +0200

    bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ description = 'Minimalistic implementation of batch Self Organizing Maps (SOM) f
 keywords = ['machine learning', 'neural networks', 'clustering', 'dimentionality reduction']
 
 setup(name='NYSom',
-      version='1.0.7',
+      version='1.0.8',
       description=description,
       long_description=long_description,
       long_description_content_type="text/markdown",
